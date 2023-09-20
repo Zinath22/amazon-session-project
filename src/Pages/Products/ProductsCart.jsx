5ffd53fad47fdd5b1484580484aab6f98ff13e55
Zinath22 <Zinath22@users.noreply.github.com>
2023-09-20 23:59:11 +0600

    cart added

--- src/Pages/Products/ProductsCart.jsx
+++ src/Pages/Products/ProductsCart.jsx
@@ -3,6 +3,17 @@ import { Link } from "react-router-dom";
 
 const ProductsCart = ({product}) => {
     const {id, images, price, stake, title} = product || {}
+    // const handleSingleItemNavigate = ()=>{
+    //     console.log('hii');
+        // const user = false;
+
+//     if (user) {
+//       navigate(`/products/${id}`);
+//     } else {
+//       navigate(`/`);
+//     }
+//   };
+    }
     // console.log(id)
     return (
         <div>
@@ -41,6 +52,13 @@ const ProductsCart = ({product}) => {
             View Details
             </button>
             </Link>
+
+             {/* <button
+              onClick={handleSingleItemNavigate}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              View Details
+            </button> */}
         </div>
     </div>
 </div>
